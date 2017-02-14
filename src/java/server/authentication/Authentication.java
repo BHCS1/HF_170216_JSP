c7a1deea34fbfbc93842f09293fc6cb94242eec5
Gabor Nagy <gabor.nagy@stylersonline.com>
2017-02-14 01:19:49 +0100

    logout

--- src/java/server/authentication/Authentication.java
+++ src/java/server/authentication/Authentication.java
@@ -1,22 +1,16 @@
 package server.authentication;
 
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.Serializable;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
-import java.util.TreeSet;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
-public class Authentication implements Serializable{
+public class Authentication {
   InputSource xmlUsers = new InputSource(getClass().getResource("users.xml").toString());
   XPath xPath = XPathFactory.newInstance().newXPath();
   MessageDigest md = null;
@@ -74,16 +68,16 @@ public class Authentication implements Serializable{
     return validAuth;
   }
 
-  public boolean hasPermission(String permission) throws XPathExpressionException, Exception {
+  public boolean hasPermission(String permission) {
     boolean hasPermission = false;
     
-//    try {
+    try {
       Node userNode = (Node)xPath.evaluate("/root/users/user[@name='"+username+"']", xmlUsers, XPathConstants.NODE);
       String role = userNode.getAttributes().getNamedItem("role").getNodeValue();
       hasPermission = (boolean)xPath.evaluate("boolean(/root/roles/role[@name='"+role+"']/permission[@name='"+permission+"'])", xmlUsers, XPathConstants.BOOLEAN);
-  //  } catch (XPathExpressionException ex) {
-    //  ex.printStackTrace();
-    //}
+    } catch (XPathExpressionException ex) {
+      ex.printStackTrace();
+    }
     
     return hasPermission;
     
